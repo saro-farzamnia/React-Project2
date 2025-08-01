c66c84434310e8e6728114bae6993b293b2455cb
saro-farzamnia <sarofarzamnia0@gmail.com>
2025-08-01 11:20:41 +0330

    project2

---
+++ React-Project2/src/context/FetchProducts.jsx
@@ -0,0 +1,11 @@
+
+
+const FetchProducts = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FetchProducts
---
+++ React-Project2/src/context/ProductList.jsx
@@ -0,0 +1,20 @@
+import { useContext } from "react"
+import { ProductsContext } from "./Products"
+
+const ProductList = () => {
+    const {products} = useContext(ProductsContext);
+    console.log(products)
+  return (
+    <div className="product-list">
+        {
+            products.map(i => (<div className="product-item" key={i.id}>
+                <img src={i.image} alt={i.description} />
+                <p>{i.category}</p>
+                <h4>{i.price}</h4>
+            </div>))
+        }      
+    </div>
+  )
+}
+
+export default ProductList
